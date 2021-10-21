1458ecf153cf3289eceaae8b04458fb29c188cc9
Dave Weekes <owndbyu@hotmail.com>
2021-10-21 12:48:45 +1000

    Progress, quest class and questing system start function

--- Zeph/ZephCore/Classes/Quest.cs
+++ Zeph/ZephCore/Classes/Quest.cs
@@ -29,11 +29,77 @@ namespace Zeph.Core.Classes {
         /// </summary>
         public Enums.QuestReceivalType q_ReceivalType = Enums.QuestReceivalType.OnceOff;
 
-        public static PlayerQuest Start(Zeph.Core.Classes.Player p, Classes.Quest q) {
-            throw new NotImplementedException();
-            //var pq = new Classes.PlayerQuest(p, q, q.questObjectives);
-            //PlayerQuest.Save(pq);
-            //return pq;
+        public List<QuestObjective> questObjectives {
+            get {
+                var lstQO = QuestObjective.Read();
+                var _questObjectives = new List<QuestObjective>();
+                foreach (var qo in lstQO) {
+                    if (qo.qo_Quest == q_ID) {
+                        _questObjectives.Add(qo);
+                    }
+                }
+                return _questObjectives;
+            }
         }
+
+        #region File Access
+
+        public new static bool Delete(int id) {
+            var db = GeneralOps.GetDatabaseConnection();
+            return db.Delete(TABLE, id);
+        }
+
+        public new static Quest Read(int id) {
+            using (var db = GeneralOps.GetDatabaseConnection()) {
+                var dic = db.Read(TABLE, id);
+                return ReadFromDictionary(dic);
+            }
+        }
+
+        public new static List<Quest> Read() {
+            var lst = new List<Quest>();
+            using (var db = GeneralOps.GetDatabaseConnection()) {
+                var _lst = db.Read(TABLE);
+                foreach (var dic in _lst) {
+                    var _dic = ReadFromDictionary(dic);
+                    if (_dic != null) {
+                        lst.Add(_dic);
+                    }
+                }
+            }
+            return lst;
+        }
+
+        public new static Quest ReadFromDictionary(Dictionary<string, object> dic) {
+            if (dic != null) {
+                try {
+                    var obj = new Quest();
+                    obj.q_ID = GeneralOps.ConvertDatabaseField<int>(dic, "id");
+                    obj.q_Name = GeneralOps.ConvertDatabaseField<string>(dic, "q_Name");
+                    obj.q_Description = GeneralOps.ConvertDatabaseField<string>(dic, "q_Description");
+                    obj.q_ObjectivesType = (Enums.QuestObjectivesType)GeneralOps.ConvertDatabaseField<int>(dic, "q_ObjectivesType");
+                    obj.q_ReceivalType = (Enums.QuestReceivalType)GeneralOps.ConvertDatabaseField<int>(dic, "q_ReceivalType");
+                    return obj;
+                } catch (Exception ex) {
+                    throw new ExceptionHandling.GeneralException("Quest", 1, "An error occurred reading dictionary " + GeneralOps.DictionaryToJson(dic) + ". " + ex.Message, ex);
+                }
+            } else {
+                return null;
+            }
+        }
+
+        public new static Quest Save(Quest obj) {
+            using (var db = GeneralOps.GetDatabaseConnection()) {
+                var dic = new Dictionary<string, object>();
+                dic["id"] = obj.q_ID;
+                dic["q_Name"] = obj.q_Name;
+                dic["q_Description"] = obj.q_Description;
+                dic["q_ObjectivesType"] = (int)obj.q_ObjectivesType;
+                dic["q_ReceivalType"] = (int)obj.q_ReceivalType;
+                return ReadFromDictionary(db.Save(TABLE, obj.q_ID, dic));
+            }
+        }
+
+        #endregion
     }
 }
