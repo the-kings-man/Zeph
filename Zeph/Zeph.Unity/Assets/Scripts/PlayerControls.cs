fdc625782faf8434030bf065e186dfffeb4a81f1
Dave Weekes <owndbyu@hotmail.com>
2021-11-20 08:08:03 +1000

    progress

--- Zeph/Zeph.Unity/Assets/Scripts/PlayerControls.cs
+++ Zeph/Zeph.Unity/Assets/Scripts/PlayerControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/PlayerControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Zeph-GameCore/PlayerControls.inputactions'
 
 using System;
 using System.Collections;
